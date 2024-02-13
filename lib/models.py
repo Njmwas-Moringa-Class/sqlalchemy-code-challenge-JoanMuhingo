7e26e81e8d6dfe340e47bd09bed6f6c0152367ed
Joan <joanmuhingo@gmail.com>
2024-02-13 23:24:37 +0300

    progress

--- lib/models.py
+++ lib/models.py
@@ -1,20 +1,28 @@
-import os
-import sys
 
-sys.path.append(os.getcwd)
 
-from sqlalchemy import (create_engine, PrimaryKeyConstraint, Column, String, Integer)
+from sqlalchemy import create_engine, PrimaryKeyConstraint, Column, String, Integer, ForeignKey
 
 from sqlalchemy.ext.declarative import declarative_base
 
+from sqlalchemy.orm import relationship, sessionmaker
 
 Base = declarative_base()
 engine = create_engine('sqlite:///db/restaurants.db', echo=True)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
-# class Review(Base):
-#     pass
+class Review(Base):
+    __tablename__ = 'reviews'
 
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    
+    customer = relationship("Customer", back_populates='reviews')
+    restaurant = relationship("Restaurant", back_populates='reviews')
+     
 class Restaurant(Base):
     __tablename__ = 'restaurants'
 
---
+++ lib/seeds.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Restaurant, Customer, Review
+
+# Connect 
+engine = create_engine('sqlite:///db/restaurants.db')
+Base.metadata.bind = engine
+
+session = DBSession()
+
+
+customer1 = Customer(first_name='John', last_name='Doe')
+customer2 = Customer(first_name='Jane', last_name='Smith')
+customer3 = Customer(first_name='Alice', last_name='Johnson')
+
+
+restaurant1 = Restaurant(name='Restaurant A', price=3)
+restaurant2 = Restaurant(name='Restaurant B', price=4)
+restaurant3 = Restaurant(name='Restaurant C', price=5)
+
+session.add_all([restaurant1, restaurant2, restaurant3])
+session.commit()
+
+
+
+session.add_all([customer1, customer2, customer3])
+session.commit()
+
+review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
+review2 = Review(star_rating=5, restaurant=restaurant2, customer=customer2)
+review3 = Review(star_rating=3, restaurant=restaurant3, customer=customer3)
+
+session.add_all([review1, review2, review3])
+session.commit()
+
+
+session.close()
